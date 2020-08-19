a996a04a434b1de367e5f09be7d458d7ea3b1641
Moritz Wallawisch <wallawitsch@gmail.com>
2020-08-19 16:13:30 +0200

    More Diamonds, more fun

--- sparkingDimons.js
+++ sparkingDimons.js
@@ -1,7 +1,13 @@
-let board = [1, 2, 3, 4, 4, 5, 5, 3, 4]
+let board = [1, 2, 3, 5, 4, 4, 5, 4, 5, 3, 4, 3, 2, 1, 2, 3]
+let board2 = [1, 2, 3, 5, 4, 4, 5, 4, 5, 3, 4, 3, 3, 5, 3, 3]
 
-const swapIndex1 = 5
-const swapIndex2 = 8
+//---------//----------//----------//----------//----------//
+
+const swapIndex1 = 6
+const swapIndex2 = 10
+
+const swapIndex1b2 = 9
+const swapIndex2b2 = 13
 
 const checkIfLegalMove = (board, swapIndex1, swapIndex2) => {
   //swapping
@@ -10,19 +16,28 @@ const checkIfLegalMove = (board, swapIndex1, swapIndex2) => {
   board[swapIndex1] = tempo
 
   //check horizontal
-  for (let i = 0; i < 9; i += 3) {
-    if (board[i] === board[i + 1] && board[i + 1] === board[i + 2]) {
-      console.log(`legal move, in horizontal line ${i}`)
+  for (let i = 0; i < 16; i += 4) {
+    if (
+      board[i] === board[i + 1] &&
+      board[i + 1] === board[i + 2] &&
+      board[i + 2] === board[i + 3]
+    ) {
+      console.log(`legal move, in horizontal line ${i / 4 + 1}`)
       return true
     }
   }
   //check vertical
-  for (let i = 0; i < 9; i += 3) {
-    if (board[i] === board[i + 3] && board[i + 3] === board[i + 6]) {
-      console.log(`legal move, in vertical line ${i}`)
+  for (let i = 0; i < 16; i += 4) {
+    if (
+      board[i] === board[i + 4] &&
+      board[i + 4] === board[i + 8] &&
+      board[i + 8] === board[i + 12]
+    ) {
+      console.log(`legal move, in vertical line ${i / 4 + 1}`)
       return true
     }
   }
 }
 
 checkIfLegalMove(board, swapIndex1, swapIndex2)
+checkIfLegalMove(board2, swapIndex1b2, swapIndex2b2)
