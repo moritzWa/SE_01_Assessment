d8cddf42b4d5c48d5e385c30e540f79f87f0af1b
Moritz Wallawisch <wallawitsch@gmail.com>
2020-08-19 15:38:58 +0200

    unoptimized matrix symmetry checking

--- sparkingDimons.js
+++ sparkingDimons.js
@@ -4,11 +4,25 @@ const swapIndex1 = 5
 const swapIndex2 = 8
 
 const checkIfLegalMove = (board, swapIndex1, swapIndex2) => {
-  console.log(board)
   //swapping
   let tempo = board[swapIndex2]
   board[swapIndex2] = board[swapIndex1]
   board[swapIndex1] = tempo
+
+  //check horizontal
+  for (let i = 0; i < 9; i += 3) {
+    if (board[i] === board[i + 1] && board[i + 1] === board[i + 2]) {
+      console.log(`legal move, in horizontal line ${i}`)
+      return true
+    }
+  }
+  //check vertical
+  for (let i = 0; i < 9; i += 3) {
+    if (board[i] === board[i + 3] && board[i + 3] === board[i + 6]) {
+      console.log(`legal move, in vertical line ${i}`)
+      return true
+    }
+  }
 }
 
 checkIfLegalMove(board, swapIndex1, swapIndex2)
